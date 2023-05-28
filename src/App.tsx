ed2d4fdc5317cb7fd6608ea8876727e694bbbd23
hayeon9826 <hayeon226@likelion.org>
2023-05-29 02:49:29 +0900

    1-1. 앱 세팅, 라우터 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Route, Routes, Navigate } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<h2>Home Page</h2>} />
+      <Route path="/posts" element={<h2>Post List Page</h2>} />
+      <Route path="/posts/:id" element={<h2>Post Detail Page</h2>} />
+      <Route path="/posts/new" element={<h2>Post New Page</h2>} />
+      <Route path="/posts/edit/:id" element={<h2>Post Edit Page</h2>} />
+      <Route path="/profile" element={<h2>Profile Page</h2>} />
+      <Route path="*" element={<Navigate replace to="/" />} />
+    </Routes>
   );
 }
 
